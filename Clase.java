d085dd3de8a485128ce60773d08c51d9d0ed41a6
SaUzumaki <100178696+SaUzumaki@users.noreply.github.com>
2023-01-23 21:14:07 -0800

    Agregado metodo personalizado

--- Clase.java
+++ Clase.java
@@ -1,7 +1,15 @@
 package Practica;
 
 public class Clase {
+	String name;
+	public Clase(String nombre) {
+		this.name=nombre;
+	}
+	public Clase() { }
 	public static String Mensaje() {
 		return "Hola";
+	}
+	public static String MensajePersonalizado(String nombre) {
+		return "Hola " + nombre;
 	}
 }
---
+++ Principal.java
@@ -0,0 +1,11 @@
+package Practica;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Clase clase = new Clase();
+		System.out.println(clase.Mensaje());
+		System.out.print(clase.MensajePersonalizado("Saul"));
+	}
+
+}
